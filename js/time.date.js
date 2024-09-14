79f56cc4a4e3beb4f706eb0d8d58122f4e3273c2
zwdemz <1941766301@qq.com>
2024-09-14 16:11:55 +0800

    Site updated: 2024-09-14 16:11:55

--- js/time.date.js
+++ js/time.date.js
@@ -1,17 +1,1 @@
-	function runtime(){
-        // 初始时间，日/月/年 时:分:秒
-        X = new Date("11/18/2022 12:32:00");
-        Y = new Date();
-        T = (Y.getTime()-X.getTime());
-        M = 24*60*60*1000;
-        a = T/M;
-        A = Math.floor(a);
-        b = (a-A)*24;
-        B = Math.floor(b);
-        c = (b-B)*60;
-        C = Math.floor((b-B)*60);
-        D = Math.floor((c-C)*60);
-        //信息写入到DIV中
-        timeDate.innerHTML = "本站勉强运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"
-    }
-    setInterval(runtime, 1000);
+function runtime(){X=new Date("11/18/2022 12:32:00");Y=new Date;T=Y.getTime()-X.getTime();M=24*60*60*1e3;a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);timeDate.innerHTML="本站勉强运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}setInterval(runtime,1e3);
